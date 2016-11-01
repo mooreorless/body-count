9ad3b200fd94a698dade8f5639896830bd2fd403
mooreorless <elliott.moore@moretonbay.qld.gov.au>
2016-11-01 11:42:11 +1000

    added initial server setup

---
+++ server/config/index.js
@@ -0,0 +1,6 @@
+export const options = {
+  url: 'https://webcamstravel.p.mashape.com/webcams/list/category=Forest/property=hd?show=webcams:location,image,url,',
+  headers: {
+    'X-Mashape-Key': 'R2uptxyNFdmshy44f3GtJ0CjqCkCp1XT71rjsn2IpT5ZYhKHxO'
+  }
+};
---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import options from './config';
+
+const router = express.Router();
+
+
+router.get('/api', (req, res) => {
+
+});
+
+// /webcams/list?show=countries
+
+export default router;
